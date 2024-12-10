67a2b51cd6ac7eed4ce9ffe9efa49048800332f3
Okigwe Ebube Ireneaus <alfredebube7@gmail.com>
2024-12-10 02:28:58 +0100

    cookie are now stored by browser

--- src/pages/Auth/SignIn.vue
+++ src/pages/Auth/SignIn.vue
@@ -12,37 +12,36 @@ const $q = useQuasar();
 const isLoading = ref(false);
 const api = inject('api');
 const { r } = route.query;
-const { user: authUser, redirect_to, isLoggedIn} = storeToRefs(useAuthStore());
+const { user: authUser, redirect_to, isLoggedIn } = storeToRefs(useAuthStore());
 if (r) {
-  redirect_to.value = r as string
-  
+  redirect_to.value = r as string;
 }
 
 const user = reactive({
   email: '',
-  psw: ''
-})
+  psw: '',
+  remember_me: false,
+});
 
 function login() {
-
   isLoading.value = true;
 
   fetch(`${api}/store/auth/login`, {
     method: 'post',
     body: JSON.stringify(user),
-	credentials: 'include',
+    credentials: 'include',
     headers: {
       'Content-Type': 'application/json',
     },
   })
     .then((res) => res.json())
     .then((data) => {
-		isLoading.value = false
       if (data.success) {
         authUser.value = data.user;
-        isLoggedIn.value = true
+        $q.cookies.set('authKey', data.token, { path: '/', expires: user.remember_me ? '30d' : '20h', sameSite: 'None', secure: true })
+        isLoggedIn.value = true;
         if (redirect_to.value) {
-          return router.push('/'+redirect_to.value);
+          return router.push('/' + redirect_to.value);
         } else {
           return router.push('/shop');
         }
@@ -55,13 +54,13 @@ function login() {
       });
     })
     .catch((error) => {
-		isLoading.value = false
       $q.notify({
         message: error.message,
         color: 'red-14',
         icon: 'error',
       });
-    });
+    })
+    .finally(() => (isLoading.value = false));
 }
 </script>
 
@@ -76,83 +75,57 @@ function login() {
             <!-- Col -->
             <div
               class="tw-w-full tw-h-auto tw-bg-gray-400 dark:tw-bg-gray-800 tw-hidden lg:tw-block lg:tw-w-5/12 tw-bg-cover tw-rounded-l-lg"
-              style="background-image: url('/img/bg-auth.jpg')"
-            ></div>
+              style="background-image: url('/img/bg-auth.jpg')"></div>
             <!-- Col -->
             <div
-              class="tw-w-full lg:tw-w-7/12 tw-bg-white dark:tw-bg-gray-700 tw-p-5 tw-rounded-lg lg:tw-rounded-l-none"
-            >
-              <h3
-                class="tw-py-4 tw-text-2xl tw-text-center tw-text-gray-800 dark:tw-text-white"
-              >
+              class="tw-w-full lg:tw-w-7/12 tw-bg-white dark:tw-bg-gray-700 tw-p-5 tw-rounded-lg lg:tw-rounded-l-none">
+              <h3 class="tw-py-4 tw-text-2xl tw-text-center tw-text-gray-800 dark:tw-text-white">
                 Create an Account!
               </h3>
-              <form
-                class="tw-px-8 tw-pt-6 tw-pb-8 tw-mb-4 tw-bg-white dark:tw-bg-gray-800 tw-rounded"
-              >
+              <form class="tw-px-8 tw-pt-6 tw-pb-8 tw-mb-4 tw-bg-white dark:tw-bg-gray-800 tw-rounded">
                 <div class="tw-mb-4">
-                  <label
-                    class="tw-block tw-mb-2 tw-text-sm tw-font-bold tw-text-gray-700 dark:tw-text-white"
-                    for="email"
-                  >
+                  <label class="tw-block tw-mb-2 tw-text-sm tw-font-bold tw-text-gray-700 dark:tw-text-white"
+                    for="email">
                     Email
                   </label>
                   <q-input
                     class="tw-w-full tw-px-3 tw-py-2 tw-mb-3 tw-text-sm tw-leading-tight tw-text-gray-700 dark:tw-text-white tw-border tw-rounded tw-shadow tw-appearance-none focus:tw-outline-none focus:tw-shadow-outline"
-                    id="email"
-                    type="email"
-                    placeholder="Email"
-                    v-model="user.email"
-                    bg-color="transparent"
-                    dense
-                    borderless
-                  />
+                    id="email" type="email" placeholder="Email" v-model="user.email" bg-color="transparent" dense
+                    borderless />
                 </div>
                 <div class="tw-mb-4">
                   <div class="">
-                    <label
-                      class="tw-block tw-mb-2 tw-text-sm tw-font-bold tw-text-gray-700 dark:tw-text-white"
-                      for="password"
-                    >
+                    <label class="tw-block tw-mb-2 tw-text-sm tw-font-bold tw-text-gray-700 dark:tw-text-white"
+                      for="password">
                       Password
                     </label>
                     <q-input
                       class="tw-w-full tw-px-3 tw-py-2 tw-mb-3 tw-text-sm tw-leading-tight tw-text-gray-700 dark:tw-text-white tw-border tw-rounded tw-shadow tw-appearance-none focus:tw-outline-none focus:tw-shadow-outline"
-                      id="password"
-                      type="password"
-                      placeholder="******************"
-                      v-model="user.psw"
-                      bg-color="transparent"
-                      dense
-                      borderless
-                    />
+                      id="password" type="password" placeholder="******************" v-model="user.psw"
+                      bg-color="transparent" dense borderless />
+                  </div>
+                </div>
+                <div class="tw-mb-1">
+                  <div class="">
+                    <q-checkbox class="tw-w-full tw-px-3 tw-py-2 tw-mb-3 tw-text-sm" label="Remember me"
+                      v-model="user.remember_me" color="accent" />
                   </div>
                 </div>
                 <div class="tw-mb-6 tw-text-center">
                   <q-btn
                     class="tw-w-full tw-px-4 tw-py-2 tw-font-bold tw-text-white bg-accent tw-rounded-full hover:tw-bg-green-800 focus:tw-outline-none focus:tw-shadow-outline"
-                    type="button"
-                    @click="login"
-                    :loading="isLoading"
-                  >
+                    type="button" @click="login" :loading="isLoading">
                     Login
                   </q-btn>
                 </div>
                 <hr class="tw-mb-6 tw-border-t" />
                 <div class="tw-text-center">
-                  <a
-                    class="tw-inline-block tw-text-sm tw-align-baseline"
-                    href="#"
-                  >
+                  <a class="tw-inline-block tw-text-sm tw-align-baseline" href="#">
                     Forgot Password?
                   </a>
                 </div>
                 <div class="tw-text-center">
-                  <router-link
-                    to="/auth/register"
-                    class="tw-inline-block tw-text-sm tw-align-baseline"
-                    
-                  >
+                  <router-link to="/auth/register" class="tw-inline-block tw-text-sm tw-align-baseline">
                     Don't have an account? Sign up
                   </router-link>
                 </div>
@@ -164,6 +137,5 @@ function login() {
     </div>
   </div>
 </template>
-
 
 <style scoped></style>
--- src/pages/Auth/SignUp.vue
+++ src/pages/Auth/SignUp.vue
@@ -15,7 +15,7 @@ const api = inject('api');
 const { r } = route.query;
 const { user: authUser, redirect_to, isLoggedIn } = storeToRefs(useAuthStore());
 if (r) {
-	redirect_to.value = r as string
+  redirect_to.value = r as string
 }
 const user = reactive({
   firstname: '',
@@ -34,19 +34,20 @@ function register() {
   fetch(`${api}/store/auth/register`, {
     method: 'put',
     body: JSON.stringify(user),
-	credentials: 'include',
+    credentials: 'include',
     headers: {
       'Content-Type': 'application/json',
     },
   })
     .then((res) => res.json())
     .then((data) => {
-		isLoading.value = false
+
       if (data.success) {
         authUser.value = data.user;
+        $q.cookies.set('authKey', data.token, { path: '/', expires: '12h', sameSite: 'None', secure: true })
         isLoggedIn.value = true
         if (redirect_to) {
-          return router.push('/'+redirect_to.value);
+          return router.push('/' + redirect_to.value);
         } else {
           return router.push('/shop');
         }
@@ -59,13 +60,13 @@ function register() {
       });
     })
     .catch((error) => {
-		isLoading.value = false
       $q.notify({
         message: error.message,
         color: 'red-14',
         icon: 'error',
       });
-    });
+    })
+    .finally(() => isLoading.value = false);
 }
 </script>
 
@@ -80,118 +81,75 @@ function register() {
             <!-- Col -->
             <div
               class="tw-w-full tw-h-auto tw-bg-gray-400 dark:tw-bg-gray-800 tw-hidden lg:tw-block lg:tw-w-5/12 tw-bg-cover tw-rounded-l-lg"
-              style="background-image: url('/img/bg-auth.jpg')"
-            ></div>
+              style="background-image: url('/img/bg-auth.jpg')"></div>
             <!-- Col -->
             <div
-              class="tw-w-full lg:tw-w-7/12 tw-bg-white dark:tw-bg-gray-700 tw-p-5 tw-rounded-lg lg:tw-rounded-l-none"
-            >
-              <h3
-                class="tw-py-4 tw-text-2xl tw-text-center tw-text-gray-800 dark:tw-text-white"
-              >
+              class="tw-w-full lg:tw-w-7/12 tw-bg-white dark:tw-bg-gray-700 tw-p-5 tw-rounded-lg lg:tw-rounded-l-none">
+              <h3 class="tw-py-4 tw-text-2xl tw-text-center tw-text-gray-800 dark:tw-text-white">
                 Create an Account!
               </h3>
-              <form
-                class="tw-px-8 tw-pt-6 tw-pb-8 tw-mb-4 tw-bg-white dark:tw-bg-gray-800 tw-rounded"
-              >
+              <form class="tw-px-8 tw-pt-6 tw-pb-8 tw-mb-4 tw-bg-white dark:tw-bg-gray-800 tw-rounded">
                 <div class="tw-mb-4 md:tw-flex md:tw-justify-between">
                   <div class="tw-mb-4 md:tw-mr-2 md:tw-mb-0">
-                    <label
-                      class="tw-block tw-mb-2 tw-text-sm tw-font-bold tw-text-gray-700 dark:tw-text-white"
-                      for="firstName"
-                    >
+                    <label class="tw-block tw-mb-2 tw-text-sm tw-font-bold tw-text-gray-700 dark:tw-text-white"
+                      for="firstName">
                       First Name
                     </label>
                     <input
                       class="tw-w-full tw-px-3 tw-py-2 tw-text-sm tw-leading-tight tw-text-gray-700 dark:tw-text-white tw-border tw-rounded tw-shadow tw-appearance-none focus:tw-outline-none focus:tw-shadow-outline"
-                      id="firstName"
-                      type="text"
-                      placeholder="First Name"
-                      v-model="user.firstname"
-                    />
+                      id="firstName" type="text" placeholder="First Name" v-model="user.firstname" />
                   </div>
                   <div class="md:tw-ml-2">
-                    <label
-                      class="tw-block tw-mb-2 tw-text-sm tw-font-bold tw-text-gray-700 dark:tw-text-white"
-                      for="lastName"
-                    >
+                    <label class="tw-block tw-mb-2 tw-text-sm tw-font-bold tw-text-gray-700 dark:tw-text-white"
+                      for="lastName">
                       Last Name
                     </label>
                     <input
                       class="tw-w-full tw-px-3 tw-py-2 tw-text-sm tw-leading-tight tw-text-gray-700 dark:tw-text-white tw-border tw-rounded tw-shadow tw-appearance-none focus:tw-outline-none focus:tw-shadow-outline"
-                      id="lastName"
-                      type="text"
-                      placeholder="Last Name"
-                      v-model="user.lastname"
-                    />
+                      id="lastName" type="text" placeholder="Last Name" v-model="user.lastname" />
                   </div>
                 </div>
                 <div class="tw-mb-4">
-                  <label
-                    class="tw-block tw-mb-2 tw-text-sm tw-font-bold tw-text-gray-700 dark:tw-text-white"
-                    for="email"
-                  >
+                  <label class="tw-block tw-mb-2 tw-text-sm tw-font-bold tw-text-gray-700 dark:tw-text-white"
+                    for="email">
                     Email
                   </label>
                   <input
                     class="tw-w-full tw-px-3 tw-py-2 tw-mb-3 tw-text-sm tw-leading-tight tw-text-gray-700 dark:tw-text-white tw-border tw-rounded tw-shadow tw-appearance-none focus:tw-outline-none focus:tw-shadow-outline"
-                    id="email"
-                    type="email"
-                    placeholder="Email"
-                    v-model="user.email"
-                  />
+                    id="email" type="email" placeholder="Email" v-model="user.email" />
                 </div>
                 <div class="tw-mb-4 md:tw-flex md:tw-justify-between">
                   <div class="tw-mb-4 md:tw-mr-2 md:tw-mb-0">
-                    <label
-                      class="tw-block tw-mb-2 tw-text-sm tw-font-bold tw-text-gray-700 dark:tw-text-white"
-                      for="password"
-                    >
+                    <label class="tw-block tw-mb-2 tw-text-sm tw-font-bold tw-text-gray-700 dark:tw-text-white"
+                      for="password">
                       Password
                     </label>
                     <input
                       class="tw-w-full tw-px-3 tw-py-2 tw-mb-3 tw-text-sm tw-leading-tight tw-text-gray-700 dark:tw-text-white tw-border tw-rounded tw-shadow tw-appearance-none focus:tw-outline-none focus:tw-shadow-outline"
-                      id="password"
-                      type="password"
-                      placeholder="******************"
-                      v-model="user.psw"
-                    />
-                    
+                      id="password" type="password" placeholder="******************" v-model="user.psw" />
+
                   </div>
                   <div class="md:tw-ml-2">
-                    <label
-                      class="tw-block tw-mb-2 tw-text-sm tw-font-bold tw-text-gray-700 dark:tw-text-white"
-                      for="c_password"
-                    >
+                    <label class="tw-block tw-mb-2 tw-text-sm tw-font-bold tw-text-gray-700 dark:tw-text-white"
+                      for="c_password">
                       Confirm Password
                     </label>
                     <input
                       class="tw-w-full tw-px-3 tw-py-2 tw-mb-3 tw-text-sm tw-leading-tight tw-text-gray-700 dark:tw-text-white tw-border tw-rounded tw-shadow tw-appearance-none focus:tw-outline-none focus:tw-shadow-outline"
-                      id="c_password"
-                      type="password"
-                      placeholder="******************"
-                      v-model="confirm"
-                    />
+                      id="c_password" type="password" placeholder="******************" v-model="confirm" />
                   </div>
                 </div>
                 <div class="tw-mb-6 tw-text-center">
                   <q-btn
                     class="tw-w-full tw-px-4 tw-py-2 tw-font-bold tw-text-white bg-accent tw-rounded-full hover:tw-bg-green-800 focus:tw-outline-none focus:tw-shadow-outline"
-                    type="button"
-                    flat
-					@click="register"
-					:loading="isLoading"
-                  >
+                    type="button" flat @click="register" :loading="isLoading">
                     Register Account
                   </q-btn>
                 </div>
                 <hr class="tw-mb-6 tw-border-t" />
 
                 <div class="tw-text-center">
-                  <router-link
-                    to="/auth/login"
-                    class="tw-inline-block tw-text-sm tw-align-baseline"
-                  >
+                  <router-link to="/auth/login" class="tw-inline-block tw-text-sm tw-align-baseline">
                     Already have an account? Login!
                   </router-link>
                 </div>
