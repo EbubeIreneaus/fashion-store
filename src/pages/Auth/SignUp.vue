d340324962bffb03882d840678e3a4084070ab0a
Okigwe Ebube Ireneaus <alfredebube7@gmail.com>
2024-12-14 00:29:31 +0100

    derk remove from auth pages

--- src/pages/Auth/SignUp.vue
+++ src/pages/Auth/SignUp.vue
@@ -72,7 +72,7 @@ function register() {
 
 <template>
   <div>
-    <div class="tw-h-full tw-bg-gray-400 dark:tw-bg-gray-900">
+    <div class="tw-h-full tw-bg-gray-400">
       <!-- Container -->
       <div class="tw-mx-auto">
         <div class="tw-flex tw-justify-center tw-px-6 tw-py-12">
@@ -80,15 +80,15 @@ function register() {
           <div class="tw-w-full xl:tw-w-3/4 lg:tw-w-11/12 tw-flex">
             <!-- Col -->
             <div
-              class="tw-w-full tw-h-auto tw-bg-gray-400 dark:tw-bg-gray-800 tw-hidden lg:tw-block lg:tw-w-5/12 tw-bg-cover tw-rounded-l-lg"
+              class="tw-w-full tw-h-auto tw-bg-gray-400 tw-hidden lg:tw-block lg:tw-w-5/12 tw-bg-cover tw-rounded-l-lg"
               style="background-image: url('/img/bg-auth.jpg')"></div>
             <!-- Col -->
             <div
-              class="tw-w-full lg:tw-w-7/12 tw-bg-white dark:tw-bg-gray-700 tw-p-5 tw-rounded-lg lg:tw-rounded-l-none">
+              class="tw-w-full lg:tw-w-7/12 tw-bg-white tw-p-5 tw-rounded-lg lg:tw-rounded-l-none">
               <h3 class="tw-py-4 tw-text-2xl tw-text-center tw-text-gray-800 dark:tw-text-white">
                 Create an Account!
               </h3>
-              <form class="tw-px-8 tw-pt-6 tw-pb-8 tw-mb-4 tw-bg-white dark:tw-bg-gray-800 tw-rounded">
+              <form class="tw-px-8 tw-pt-6 tw-pb-8 tw-mb-4 tw-bg-white tw-rounded">
                 <div class="tw-mb-4 md:tw-flex md:tw-justify-between">
                   <div class="tw-mb-4 md:tw-mr-2 md:tw-mb-0">
                     <label class="tw-block tw-mb-2 tw-text-sm tw-font-bold tw-text-gray-700 dark:tw-text-white"
@@ -96,7 +96,7 @@ function register() {
                       First Name
                     </label>
                     <input
-                      class="tw-w-full tw-px-3 tw-py-2 tw-text-sm tw-leading-tight tw-text-gray-700 dark:tw-text-white tw-border tw-rounded tw-shadow tw-appearance-none focus:tw-outline-none focus:tw-shadow-outline"
+                      class="tw-w-full tw-px-3 tw-py-2 tw-text-sm tw-leading-tight tw-text-gray-700 tw-border tw-rounded tw-shadow tw-appearance-none focus:tw-outline-none focus:tw-shadow-outline"
                       id="firstName" type="text" placeholder="First Name" v-model="user.firstname" />
                   </div>
                   <div class="md:tw-ml-2">
@@ -105,37 +105,37 @@ function register() {
                       Last Name
                     </label>
                     <input
-                      class="tw-w-full tw-px-3 tw-py-2 tw-text-sm tw-leading-tight tw-text-gray-700 dark:tw-text-white tw-border tw-rounded tw-shadow tw-appearance-none focus:tw-outline-none focus:tw-shadow-outline"
+                      class="tw-w-full tw-px-3 tw-py-2 tw-text-sm tw-leading-tight tw-text-gray-700 tw-border tw-rounded tw-shadow tw-appearance-none focus:tw-outline-none focus:tw-shadow-outline"
                       id="lastName" type="text" placeholder="Last Name" v-model="user.lastname" />
                   </div>
                 </div>
                 <div class="tw-mb-4">
-                  <label class="tw-block tw-mb-2 tw-text-sm tw-font-bold tw-text-gray-700 dark:tw-text-white"
+                  <label class="tw-block tw-mb-2 tw-text-sm tw-font-bold tw-text-gray-700"
                     for="email">
                     Email
                   </label>
                   <input
-                    class="tw-w-full tw-px-3 tw-py-2 tw-mb-3 tw-text-sm tw-leading-tight tw-text-gray-700 dark:tw-text-white tw-border tw-rounded tw-shadow tw-appearance-none focus:tw-outline-none focus:tw-shadow-outline"
+                    class="tw-w-full tw-px-3 tw-py-2 tw-mb-3 tw-text-sm tw-leading-tight tw-text-gray-700 tw-border tw-rounded tw-shadow tw-appearance-none focus:tw-outline-none focus:tw-shadow-outline"
                     id="email" type="email" placeholder="Email" v-model="user.email" />
                 </div>
                 <div class="tw-mb-4 md:tw-flex md:tw-justify-between">
                   <div class="tw-mb-4 md:tw-mr-2 md:tw-mb-0">
-                    <label class="tw-block tw-mb-2 tw-text-sm tw-font-bold tw-text-gray-700 dark:tw-text-white"
+                    <label class="tw-block tw-mb-2 tw-text-sm tw-font-bold tw-text-gray-700"
                       for="password">
                       Password
                     </label>
                     <input
-                      class="tw-w-full tw-px-3 tw-py-2 tw-mb-3 tw-text-sm tw-leading-tight tw-text-gray-700 dark:tw-text-white tw-border tw-rounded tw-shadow tw-appearance-none focus:tw-outline-none focus:tw-shadow-outline"
+                      class="tw-w-full tw-px-3 tw-py-2 tw-mb-3 tw-text-sm tw-leading-tight tw-text-gray-700 tw-border tw-rounded tw-shadow tw-appearance-none focus:tw-outline-none focus:tw-shadow-outline"
                       id="password" type="password" placeholder="******************" v-model="user.psw" />
 
                   </div>
                   <div class="md:tw-ml-2">
-                    <label class="tw-block tw-mb-2 tw-text-sm tw-font-bold tw-text-gray-700 dark:tw-text-white"
+                    <label class="tw-block tw-mb-2 tw-text-sm tw-font-bold tw-text-gray-700"
                       for="c_password">
                       Confirm Password
                     </label>
                     <input
-                      class="tw-w-full tw-px-3 tw-py-2 tw-mb-3 tw-text-sm tw-leading-tight tw-text-gray-700 dark:tw-text-white tw-border tw-rounded tw-shadow tw-appearance-none focus:tw-outline-none focus:tw-shadow-outline"
+                      class="tw-w-full tw-px-3 tw-py-2 tw-mb-3 tw-text-sm tw-leading-tight tw-text-gray-700 tw-border tw-rounded tw-shadow tw-appearance-none focus:tw-outline-none focus:tw-shadow-outline"
                       id="c_password" type="password" placeholder="******************" v-model="confirm" />
                   </div>
                 </div>
